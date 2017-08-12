71ff95b5d46b268af7055cccd1b7d6aff970186f
Mora <Mora@DESKTOP-7L8U1TK>
2017-08-11 19:05:47 -0600

    Se agrego el Primer Metodo

--- Ulatina.PrograAvanzada.2ExamParcial/Ulatina.PrograAvanzada.AW.WCF/Dominio/Interfaz/IService1.cs
+++ Ulatina.PrograAvanzada.2ExamParcial/Ulatina.PrograAvanzada.AW.WCF/Dominio/Interfaz/IService1.cs
@@ -7,6 +7,8 @@ using System.ServiceModel.Web;
 using System.Text;
 
 namespace Ulatina.PrograAvanzada.AW.WCF
+
+
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
@@ -19,10 +21,35 @@ namespace Ulatina.PrograAvanzada.AW.WCF
         [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
-        // TODO: agregue aquí sus operaciones de servicio
+
+
+        [OperationContract]
+        Model.SalesOrderHeader FaturasRanFecha(int fecha);
     }
 
 
+
+    //    a.Lista de facturas por rango de fecha de la orden.
+
+    //b.Lista de facturas por rango de total.
+
+
+    //c.Lista de facturas que incluyan al menos una línea de detalle cuyo monto total de descuento exceda un monto en específico.
+
+    //d.Lista de facturas que incluyan al menos una línea de detalle cuya cantidad de artículos de la orden esté en un rango específico.
+
+    //e.Lista de facturas de un género específico de vendedor.
+
+    //f.Lista de facturas de vendedores que tengan un rango específico de edad.
+
+    //g.Lista de facturas de vendedores que tengan un rango específico de antigüedad de trabajo en la empresa.
+
+    //h.Lista de facturas de vendedores que tengan un texto específico en el apellido o en el nombre.
+
+
+  
+
+        
     // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
     [DataContract]
     public class CompositeType
--- Ulatina.PrograAvanzada.2ExamParcial/Ulatina.PrograAvanzada.AW.WCF/Dominio/Servicios/Service1.svc.cs
+++ Ulatina.PrograAvanzada.2ExamParcial/Ulatina.PrograAvanzada.AW.WCF/Dominio/Servicios/Service1.svc.cs
@@ -5,13 +5,22 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using Ulatina.PrograAvanzada.AW.Model;
 
 namespace Ulatina.PrograAvanzada.AW.WCF
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class Service1 : IService1
+
+
+
     {
+        public SalesOrderHeader FaturasRanFecha(int fecha)
+        {
+            throw new NotImplementedException();
+        }
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
